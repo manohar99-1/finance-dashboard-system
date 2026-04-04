a3564751a6f21868c36c500367157d28cd2e5041
manohar99-1 <124374054+manohar99-1@users.noreply.github.com>
2026-04-04 11:25:37 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, DateTime, Text, Enum
-from sqlalchemy.orm import declarative_basefrom sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
 import enum
 
